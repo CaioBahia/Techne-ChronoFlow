d7c2aba47f08ce2049bf69feadb2d7cc4da88091
Caio Bahia <caiobahia.dev@gmail.com>
2025-09-25 10:28:23 -0300

    feat: SSE backend date format update

--- backend/src/main/java/com/techne/ChronoFlow/application/dto/JobStatusUpdateDTO.java
+++ backend/src/main/java/com/techne/ChronoFlow/application/dto/JobStatusUpdateDTO.java
@@ -1,5 +1,6 @@
 package com.techne.ChronoFlow.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.techne.ChronoFlow.domain.job.Job;
 import com.techne.ChronoFlow.domain.job.enums.JobStatus;
 import lombok.Getter;
@@ -12,7 +13,11 @@ import java.time.LocalDateTime;
 public class JobStatusUpdateDTO {
     private Long id;
     private JobStatus status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime proximaExecucao;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime ultimaExecucao;
 
     public JobStatusUpdateDTO(Job job) {
