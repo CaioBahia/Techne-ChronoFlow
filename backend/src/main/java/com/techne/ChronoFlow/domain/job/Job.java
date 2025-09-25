41b24bfc90652a0cce8535d47cb0c4aacde634e2
Caio Bahia <caiobahia.dev@gmail.com>
2025-09-24 21:06:43 -0300

    feat: (File Processing) bug correction

--- backend/src/main/java/com/techne/ChronoFlow/domain/job/Job.java
+++ backend/src/main/java/com/techne/ChronoFlow/domain/job/Job.java
@@ -1,12 +1,21 @@
 package com.techne.ChronoFlow.domain.job;
 
-import com.techne.ChronoFlow.domain.job.enums.Empresas;
+import com.techne.ChronoFlow.domain.job.enums.Empresa;
 import com.techne.ChronoFlow.domain.job.enums.JobStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "JOB")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Job {
 
     @Id
@@ -25,67 +34,9 @@ public class Job {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Empresas empresas;
+    private Empresa empresa;
 
     private LocalDateTime ultimaExecucao;
 
     private LocalDateTime proximaExecucao;
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCronExpression() {
-        return cronExpression;
-    }
-
-    public void setCronExpression(String cronExpression) {
-        this.cronExpression = cronExpression;
-    }
-
-    public JobStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(JobStatus status) {
-        this.status = status;
-    }
-
-    public Empresas getEmpresa() {
-        return empresas;
-    }
-
-    public void setEmpresa(Empresas empresas) {
-        this.empresas = empresas;
-    }
-
-    public LocalDateTime getUltimaExecucao() {
-        return ultimaExecucao;
-    }
-
-    public void setUltimaExecucao(LocalDateTime ultimaExecucao) {
-        this.ultimaExecucao = ultimaExecucao;
-    }
-
-    public LocalDateTime getProximaExecucao() {
-        return proximaExecucao;
-    }
-
-    public void setProximaExecucao(LocalDateTime proximaExecucao) {
-        this.proximaExecucao = proximaExecucao;
-    }
 }
--- frontend/chrono_flow/src/app/app.routes.ts
+++ frontend/chrono_flow/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
-import { JobListComponent } from './jobs/job-list/job-list.component';
+import { JobListComponent } from './jobs/components/job-list/job-list.component';
 
 export const routes: Routes = [
     { path: '', component: JobListComponent }
